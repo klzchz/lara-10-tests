1518832eee4da0577d4b5984a402cd911b0e834f
Lucas Andrade <lucascorporation@live.com>
2023-03-21 22:35:16 -0300

    teste listando com o Blade

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,21 @@
 <h1>Listagem de Suportes</h1>
+
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>status</th>
+        <th>Descrição</th>
+        <th></th>
+    </thead>
+    <tbody>
+        <tr>
+            @foreach($supports->where('status','p') as $support)
+            <td>{{$support->subject}}</td> 
+            <td>{{$support->status}}</td>    
+            <td>{{$support->body}}</td>       
+            <td>></td>
+            @endforeach
+        </tr>
+    </tbody>
+</table>
